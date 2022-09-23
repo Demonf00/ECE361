63dfc2f7622561bcbd34b9920e56e841aba0a0df
Frederick Zhao <freddy.zhao@mail.utoronto.ca>
2022-09-23 14:20:56 -0400

    Added the error input for server

--- lab1/deliver.c
+++ lab1/deliver.c
@@ -1,4 +1,10 @@
-#include <stdio.h>
+#include<sys/types.h>//structure
+#include<sys/socket.h>
+#include<stdio.h>
+#include<netinet/in.h>//adress
+#include<arpa/inet.h>//format convert
+#include<stdlib.h>
+#include<string.h>
 
 int main(int argc, char* argv[])
 {
--- lab1/server.c
+++ lab1/server.c
@@ -1,7 +1,19 @@
-#include <stdio.h>
+#include<sys/types.h>//structure
+#include<sys/socket.h>
+#include<stdio.h>
+#include<netinet/in.h>//adress
+#include<arpa/inet.h>//format convert
+#include<stdlib.h>
+#include<string.h>
 
 int main(int argc, char* argv[])
 {
+    if (argc != 2)
+    {
+        fprintf(stderr, "Missing port number or too many arguments!\nThe correct usage is: server port\n");
+	    exit(1);
+    }
+    int port = atoi(argv[1]);
     printf("hello world\n");
     return 0;
 }
