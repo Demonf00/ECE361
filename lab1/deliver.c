6cceacc09248427395f95994bd4cf703b5a6233c
Tracy Sun <tracy.sun@mail.utoronto.ca>
2022-09-29 23:05:38 -0400

    updated deliver.c

--- lab1/deliver.c
+++ lab1/deliver.c
@@ -1,85 +1,74 @@
-// Client side implementation of UDP client-server model 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <fcntl.h>
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
-    
-    
-// Driver code 
-int main(int argc, char *argv[]) {
-    if (argc != 3)
-    {
-        fprintf(stderr, "Missing or too many arguments!\nThe correct usage should be: deliver address port\n");
-	    exit(1);
-    }
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
-    int port = atoi(argv[2]);
-
-    // printf("%d\n", port);
-    int sockfd; 
-    char buffer[BUFSIZ]; 
-    char command[BUFSIZ];
-    char filepath[BUFSIZ];
-    char *ftp = "ftp";
-    char *yes = "yes";
-    struct sockaddr_in     servaddr; 
+int main(int argc, char *argv[]){
+    int socket_desc;
+    struct sockaddr_in server_addr;
+    char server_message[2000], client_message[2000];
+    int server_struct_length = sizeof(server_addr);
     
-    // Creating socket file descriptor 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-        fprintf(stderr, "Socket error!\n");
-	    exit(1);
-    } 
-    
-    memset(&servaddr, 0, sizeof(servaddr)); 
-        
-    // Filling server information 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_port = htons(port); 
-    servaddr.sin_addr.s_addr = inet_addr(argv[1]); 
-        
-    int n, len; 
-    
-    printf("Client: Please input a message as follows: ftp <file name>\n");
-    scanf("%s", command);
-    scanf("%s", filepath);
- 
-    if (strcmp(command, ftp)!=0)
-    {
-        fprintf(stderr, "Wrong command! Please input as follows: ftp <filename>\n");
+    // deliever ip_address port
+    if (argc != 3) {
+        printf("argument format incorrect  %d\n", argc);
         exit(1);
     }
-
-    int file = open(filepath, O_RDONLY);
-    if (file < 0)
-    {
-        fprintf(stderr, "File %s does not exist!\n", filepath);
-	    exit(1);
+    
+    // Create IPv4 UDP socket:
+    socket_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    
+    if(socket_desc < 0){
+        printf("Error while creating socket\n");
+        return -1;
+    }
+    
+    // Set port and IP:
+    server_addr.sin_family = AF_INET; // IPv4
+    server_addr.sin_port = htons(atoi(argv[2]));
+    server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    
+    printf("deliver <%s> <%d>\n", inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
+    while(1){
+    // Clean buffers:
+    memset(server_message, '\0', sizeof(server_message));
+    memset(client_message, '\0', sizeof(client_message));    
+        
+    // Get input from the user:
+    printf("Enter 'ftp <file name>' ");
+    gets(client_message);
+    
+    // check input format and if file exist
+    char *message = strtok(client_message, " ");
+    if(strcmp(message,"ftp")==0 ){
+        message = strtok(NULL, " ");
+        if(access(message, F_OK)!=0){
+            printf("file not exist. \n");
+            continue; 
+        }            
+    }       
+    else{
+        printf("Incorrect format.\n");
+        continue;
+    }
+        
+    
+    // Send the message to server:
+    if(sendto(socket_desc, "ftp", strlen("ftp"), 0,
+         (struct sockaddr*)&server_addr, server_struct_length) < 0){
+        printf("Unable to send message\n");
+        return -1;
+    }
+    printf("ftp sent to server\n");
+    // Receive the server's response:
+    recvfrom(socket_desc, server_message, sizeof(server_message), 0,(struct sockaddr*)&server_addr, &server_struct_length);
+    
+    if(strcmp(server_message,"yes")==0)
+        printf("A file transfer can start\n");
+    else
+        break;
     }
 
-    sendto(sockfd, (const char *)ftp, strlen(ftp), 
-        MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
-            sizeof(servaddr)); 
-    printf("Client: %s %s message sent to %s:%d.\n", command, filepath, argv[1], port);
-            
-    n = recvfrom(sockfd, (char *)buffer, BUFSIZ,  
-                MSG_WAITALL, (struct sockaddr *) &servaddr, 
-                &len);
-
-    printf("Client: Received message from server %s:%d %s\n", argv[1], port, buffer); 
-    buffer[n] = '\0';
-    if (strcmp(buffer, yes) == 0) 
-        printf("Client: A file transfer can start\n");
-
-    else{
-        fprintf(stderr, "Please try again!\n");
-	    exit(1);
-    } 
-    
-    close(sockfd); 
-    return 0; 
 }
+
