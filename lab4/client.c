2a1f9f02ff2a404fafa9924b30c10aa0ecda067f
Tracy Sun <tracy.sun@mail.utoronto.ca>
2022-11-16 03:31:00 -0500

    finish client side

--- lab4/client.c
+++ lab4/client.c
@@ -1,7 +1,243 @@
 #include "conference.h"
 
+// https://www.geeksforgeeks.org/tcp-server-client-implementation-in-c/
+// https://aticleworld.com/socket-programming-in-c-using-tcpip/
+
+#include <strings.h> // bzero()
+
+#define MAX_CMD 80
+
+char commands[MAX_CMD], input[2000];
+int sock_desc, connfd;
+struct sockaddr_in servaddr;
+struct message msg, response;
+char server_message[5000], client_message[5000];
+
+void encode(){
+    bzero(client_message,sizeof(client_message));
+    snprintf(client_message, sizeof(client_message),
+    "%d;%d;%s;%s",msg.type,msg.size,msg.source,msg.data);
+}
+void decode(){
+    bzero(response.data,MAX_DATA);
+    char *item  = strtok(server_message, ";");
+    char *contain[4];
+    int i=0;
+    while(item != NULL){
+        contain[i++]=item;
+        item  = strtok(NULL, ":");
+    }
+    response.type = atoi(contain[0]);
+    response.size = atoi(contain[1]);
+    strcpy(response.source,contain[2]);
+    strcpy(response.data,contain[3]);
+
+    bzero(server_message,sizeof(server_message));
+
+}
+
+void text_recv(){
+    printf("%s\n",response.data);
+}
+
 int main()
 {
-    
-    return 0;
+
+    while (true)
+    {
+        fgets(input, 2000, stdin);
+
+        char *command = strtok(input, " \n");
+
+        printf("%s\n", command);
+
+        if (strcmp(command, "/login") == 0)
+        {
+            // /login <client ID> <password> <server-IP> <server-port>
+            printf("in login mode\n");
+
+            char *info[4];
+            int i=0;
+            command  = strtok(NULL, " ");
+            while(command != NULL&&i<4){
+                printf("%d\n",i);
+                info[i++]=command;
+                command  = strtok(NULL, " ");
+            }
+            if(i<3||command != NULL){
+                printf("number of agument incorrect\n");
+                continue;
+            }
+            char *name = info[0];
+            char *password=info[1]; 
+
+            // using default protocol->TCP
+            sock_desc = socket(AF_INET, SOCK_STREAM, 0);
+            if (sock_desc == -1){
+                printf("socket creation failed\n");
+                return -1;
+            }
+
+            // clear server socket info
+            bzero(&servaddr, sizeof(servaddr));
+
+            // assign IP, PORT
+            servaddr.sin_family = AF_INET;
+            servaddr.sin_addr.s_addr = inet_addr(info[2]);
+            servaddr.sin_port = htons(atoi(info[3]));
+
+            // connect the client socket to server socket
+            if (connect(sock_desc, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0)
+            {
+                printf("connection with the server failed\n");
+                return -1;
+            }
+
+            
+            msg.type = LOGIN;
+            msg.size = strlen(info[1]);
+            strcpy(msg.source,info[0]);
+            strcpy(msg.data,info[1]);
+
+            encode();
+            write(sock_desc, client_message, sizeof(client_message));
+            
+            read(sock_desc, server_message, sizeof(server_message));
+            decode();
+            while(response.type==MESSAGE){
+                text_recv();
+                read(sock_desc, server_message, sizeof(server_message));
+                decode();
+            }
+            if(response.type!=LO_ACK){
+                printf("login not success\n");
+                if(response.type==LO_NAK)
+                    printf("%s\n", response.data);
+            }
+
+        }
+        else if (strcmp(command, "/logout") == 0){
+            //reset client
+            printf("in logout mode\n");
+
+            bzero(msg.data,MAX_DATA);
+            msg.type = EXIT;
+            msg.size = 0;
+
+            encode();
+            write(sock_desc, client_message, sizeof(client_message));
+            
+            close(sock_desc);
+        }
+        else if (strcmp(command, "/joinsession>") == 0){
+            // /joinsession <session ID>
+            printf("in joinsession mode\n");
+            command  = strtok(NULL, " ");
+            char *session_id = command;
+
+            bzero(msg.data,MAX_DATA);
+            msg.type = JOIN;
+            msg.size = strlen(command);
+            strcpy(msg.data,command);
+
+            encode();
+            write(sock_desc, client_message, sizeof(client_message));
+            
+            read(sock_desc, server_message, sizeof(server_message));
+            decode();
+            while(response.type==MESSAGE){
+                text_recv();
+                read(sock_desc, server_message, sizeof(server_message));
+                decode();
+            }
+            if(response.type!=JN_ACK){
+                    printf("join not success\n");
+                    if(response.type==JN_NAK)
+                        printf("reason: %s\n", response.data);
+            }
+        }
+        else if (strcmp(command, "/leavesession") == 0){
+            // leave current session
+            printf("in leavesession mode\n");
+
+            bzero(msg.data,MAX_DATA);
+            msg.type = LEAVE_SESS;
+            msg.size = 0;
+
+            encode();
+            write(sock_desc, client_message, sizeof(client_message));
+
+        }
+        else if (strcmp(command, "/createsession") == 0){
+            // /createsession <session ID> Create and join new session
+            printf("in createsession mode\n");
+
+            command  = strtok(NULL, " ");
+
+            bzero(msg.data,MAX_DATA);
+            msg.type = NEW_SESS;
+            msg.size = strlen(command);
+            strcpy(msg.data,command);
+
+            encode();
+            write(sock_desc, client_message, sizeof(client_message));
+            
+            read(sock_desc, server_message, sizeof(server_message));
+            decode();
+            while(response.type==MESSAGE){
+                text_recv();
+                read(sock_desc, server_message, sizeof(server_message));
+                decode();
+            }
+
+            if(response.type!=NS_ACK)
+                    printf("create not success\n");  
+
+        }
+        else if (strcmp(command, "/list") == 0){
+            // Get the list of the connected clients and available sessions
+            printf("in list mode\n");
+            bzero(msg.data,MAX_DATA);
+            msg.type = QUERY;
+            msg.size = 0;
+
+            encode();
+            write(sock_desc, client_message, sizeof(client_message));
+            
+            read(sock_desc, server_message, sizeof(server_message));
+            decode();
+
+            while(response.type==MESSAGE){
+                text_recv();
+                read(sock_desc, server_message, sizeof(server_message));
+                decode();
+            }
+            if(response.type!=QU_ACK)
+                    printf("get list not success\n"); 
+            else{
+                char * user = strtok(response.data, " ");
+                while( user != NULL ) {
+                    printf( "* %s\n", user );
+                    user = strtok(NULL, " ");
+                }
+            }
+
+        }
+        else if (strcmp(command, "/quit") == 0){
+            // Terminate the program
+            return (0);
+        }
+        else{
+            printf("%s\n", input);
+            // bzero(msg.data,MAX_DATA);
+            // msg.type = MESSAGE;
+            // msg.size = strlen(command);
+            // msg.data = command;
+
+            // write(sock_desc, msg, sizeof(msg));
+        }
+    }
+
+    // close the socket
+    // close(sockfd);
 }
